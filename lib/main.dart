f0cf0fb4f6a73c8eb37473ddbb37f8681e1cb16f
sameeranand12 <201951134@iiitvadodara.ac.in>
2021-05-24 05:05:48 +0530

    Fonts and pics added

--- lib/main.dart
+++ lib/main.dart
@@ -19,13 +19,23 @@ class MyApp extends StatelessWidget {
             //mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(radius: 50.0, backgroundImage:AssetImage('images/sameer.jpg'),),
-              Text(
-                  'Sameer Anand',
-                    style: TextStyle(
-                      fontSize: 30.0, fontWeight:FontWeight.bold
+              Text('Sameer Anand',
+                    style: TextStyle(fontFamily: 'Pacifico',
+                       fontSize: 30.0,
+                        color:Colors.white,
+                       fontWeight:FontWeight.bold
                     ),
+              ),
+              Container(
+                 child: Text ('FLUTTER DEVELOPER',
+                  style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                  color:Colors.teal.shade100,
+                    letterSpacing: 2.5
 
-                  ),
+                ), ),
+              ),
             ],
           ),
         ),
